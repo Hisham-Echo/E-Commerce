08e2e8d619800f1e58f91fb0a4478e754997bf56
Hisham-Echo <hishamsaleh6090@gmail.com>
2025-09-07 07:56:16 +0300

    Add initial project files

---
+++ front-end/src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./main.css";
+import * as VueRouter from "vue-router";
+import ShoppingCartPage from "./pages/ShoppingCartPage.vue";
+import ProductsPage from "./pages/ProductsPage.vue";
+import ProductDetailPage from "./pages/ProductDetailPage.vue";
+
+createApp(App)
+    .use(
+        VueRouter.createRouter({
+            history: VueRouter.createWebHistory(process.env.BASE_URL),
+            routes: [
+                { path: "/", component: App },
+                { path: "/cart", component: ShoppingCartPage },
+                { path: "/products", component: ProductsPage },
+                { path: "/product/:productId", component: ProductDetailPage },
+            ],
+        })
+    )
+    .mount("#app");
